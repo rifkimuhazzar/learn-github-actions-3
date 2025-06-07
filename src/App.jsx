de5c08b28fc813f7f1fbe79e948f22447251200e
Rifki <rifkiki78@gmail.com>
2025-06-07 10:50:17 +0700

    add comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import MainContent from './components/MainContent';
-import logo from './assets/images/logo.png';
+import MainContent from "./components/MainContent";
+import logo from "./assets/images/logo.png";
 
+// Hello
 function App() {
   return (
     <>
